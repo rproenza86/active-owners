c40be7054f91f7c71f4d079cfb18737ff63ae0b7
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2020-01-30 16:58:09 -0500

    feat: Implement notification subscription db logic
    The notification crud ops are done on the commit.

--- src/services/commonDbOps.ts
+++ src/services/commonDbOps.ts
@@ -1,5 +1,5 @@
 import { db } from '../firebase';
-import { IDBOpsSimpleResult } from '../types';
+import { IDBOpsSimpleResult, IUserSubscription } from '../types';
 
 export async function getDocument<T>(docName: string): Promise<T[]> {
     return db
@@ -73,6 +73,100 @@ export async function deleteDocument<T>(
         })
         .catch(function(error) {
             const errorMsg = `Error deleting document: ${docId}`;
+            console.error(errorMsg, error);
+            return { ok: true, msg: errorMsg };
+        });
+}
+
+export async function subscribeToAONotification<T>(
+    ofTeamId: string,
+    userSubscription: IUserSubscription
+): Promise<IDBOpsSimpleResult> {
+    return db
+        .collection('teamsNotifications')
+        .doc(ofTeamId)
+        .collection('activeOwnerUpdates')
+        .doc(userSubscription.uid)
+        .set(userSubscription)
+        .then(() => {
+            console.log('Subscription written with ID: ', userSubscription.uid);
+            const msg = 'Subscription successfully updated!';
+            console.log(msg);
+            return { ok: true, msg };
+        })
+        .catch(function(error) {
+            const errorMsg = 'Error updating document: ';
+            console.error(errorMsg, error);
+            return { ok: true, msg: errorMsg };
+        });
+}
+
+export async function updateUserAONotificationSubscription<T>(
+    ofTeamId: string,
+    userSubscription: IUserSubscription
+): Promise<IDBOpsSimpleResult> {
+    return db
+        .collection('teamsNotifications')
+        .doc(ofTeamId)
+        .collection('activeOwnerUpdates')
+        .doc(userSubscription.uid)
+        .update(userSubscription)
+        .then(() => {
+            console.log('Subscription written with ID: ', userSubscription.uid);
+            const msg = 'Subscription successfully updated!';
+            console.log(msg);
+            return { ok: true, msg };
+        })
+        .catch(function(error) {
+            const errorMsg = 'Error updating document: ';
+            console.error(errorMsg, error);
+            return { ok: true, msg: errorMsg };
+        });
+}
+
+export async function getTeamsAOSubscriptions<T>(forUserId: string): Promise<T[]> {
+    try {
+        const data: T[] = [];
+
+        const querySnapshot = await db
+            .collectionGroup('activeOwnerUpdates')
+            .where('uid', '==', forUserId)
+            .orderBy('email')
+            .get();
+
+        for (const doc of querySnapshot.docs) {
+            const entry: T = ({
+                id: doc.id,
+                ...doc.data()
+            } as unknown) as T;
+            data.push(entry);
+        }
+
+        return data;
+    } catch (error) {
+        const errorMsg = `Error getting Subscriptions documents.`;
+        console.error(errorMsg, error);
+        return [];
+    }
+}
+
+export async function unsubscribeToAONotification<T>(
+    uid: string,
+    teamId: string
+): Promise<IDBOpsSimpleResult> {
+    return db
+        .collection('teamsNotifications')
+        .doc(teamId)
+        .collection('activeOwnerUpdates')
+        .doc(uid)
+        .delete()
+        .then(() => {
+            const msg = 'Subscription successfully deleted!';
+            console.log(msg);
+            return { ok: true, msg };
+        })
+        .catch(function(error: any) {
+            const errorMsg = `Error deleting Subscription document: teamId ${teamId} / uid ${uid}`;
             console.error(errorMsg, error);
             return { ok: true, msg: errorMsg };
         });
--- src/services/teams.ts
+++ src/services/teams.ts
@@ -1,7 +1,30 @@
-import { ITeam, IDBOpsSimpleResult } from '../types';
-import { getDocument, updateDocument } from './commonDbOps';
+import { ITeam, IDBOpsSimpleResult, IUserSubscription } from '../types';
+import {
+    getDocument,
+    updateDocument,
+    subscribeToAONotification,
+    getTeamsAOSubscriptions,
+    unsubscribeToAONotification
+} from './commonDbOps';
 
 export const getTeams = (): Promise<ITeam[]> => getDocument<ITeam>('teams');
 
 export const updateTeam = (docId: string, updateObj: any): Promise<IDBOpsSimpleResult> =>
     updateDocument('teams', docId, updateObj);
+
+export const getUserSubscriptions = async (uid: string, teams: ITeam[]) => {
+    const subscriptions: IUserSubscription[] = await getTeamsAOSubscriptions<IUserSubscription>(
+        uid
+    );
+    return subscriptions;
+};
+
+export const notifyUserOnAOUpdatesBy = (
+    teamId: string,
+    withUserSubscription: IUserSubscription
+): Promise<IDBOpsSimpleResult> => subscribeToAONotification(teamId, withUserSubscription);
+
+export const unsubscribeUserOfTeamACUpdates = (
+    uid: string,
+    teamId: string
+): Promise<IDBOpsSimpleResult> => unsubscribeToAONotification(uid, teamId);
