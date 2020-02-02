721c89e0761ea49098a14b8727b6b418f3332742
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2020-02-02 14:21:35 -0500

    chore: Middleware registration

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -4,9 +4,10 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { verifyAuth } from './actions';
 import rootReducer from './reducers';
+import { cloudMessagingMiddleware } from './middleware/cloudMessaging';
 
 export default function configureStore(persistedState?: any) {
-    const middlewares = [thunkMiddleware];
+    const middlewares = [thunkMiddleware, cloudMessagingMiddleware];
     const middlewareEnhancer = applyMiddleware(...middlewares);
 
     const enhancers = [middlewareEnhancer];
